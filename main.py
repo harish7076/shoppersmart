3a38a46b249c6b0e498b18e1a3cc983ae4be6dc9
harish7076 <harishchavan7076@gmail.com>
2025-07-17 20:36:56 +0530

    main

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from routes.customer_routes import customer_n
+from routes.product_routes import product_n
+from routes.transaction_routes import transaction_n
+from database import Base, engine
+from models.customer import Customer
+from models.product import Product
+from models.transaction import Transaction
+
+app = Flask(__name__)
+
+app.register_blueprint(customer_n)
+app.register_blueprint(product_n)
+app.register_blueprint(transaction_n)
+
+# Create tables if they don't exist
+Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    app.run(debug=True)
